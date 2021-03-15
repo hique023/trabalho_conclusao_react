0ecd4e673d4132f3d924668b6ddb693100f4917a
hique023 <pedroviski10@hotmail.com>
2021-03-14 22:20:39 -0300

    Realizado map para renderizar os fornecedores

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,12 @@
 // Global
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
+import ListProvider from '../ListProvider'
 
 // Assets
 import { AiOutlineLogout } from 'react-icons/ai'
-import { FiTrash2 } from 'react-icons/fi'
-import { RiEdit2Line } from 'react-icons/ri'
 import logopage from '../../assets/logoProject.png'
+import { useState } from 'react'
 
 const Site = styled.div`
   width: 100%;
@@ -109,39 +109,10 @@ const Linkinside = styled.h3`
   transition: opacity 0.2s;
 `
 
-const DivListProvider = styled.div`
-  display: flex;
-  align-itens: center;
-  justify-content: center;
-`
-
-const ListProvider = styled.ul`
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  grid-gap: 24px;
-  list-style: none;
-  margin-bottom: 50px;
-`
-
-const ButtonDelete = styled.button`
-  position: absolute;
-  right: 24px;
-  top: 24px;
-  border: 0;
-  transition: opacity 0.2s;
-  background: transparent;
-`
-
-const ButtonEdit = styled.button`
-  position: absolute;
-  right: 60px;
-  top: 24px;
-  border: 0;
-  transition: opacity 0.2s;
-  background: transparent;
-`
-
 export default function Home() {
+
+  const [providers, setProviders] = useState([{}, {}, {}, {}, {}, {}, {}])
+
   return (
     <Site>
       <Header>
@@ -167,119 +138,20 @@ export default function Home() {
         </DivSuperior>
       </Container>
 
-      <DivListProvider>
-        <ListProvider>
-          <li>
-            <strong>Nome do fornecedor</strong>
-            <p>Fornecedor teste</p>
+      <ListProvider />
+      <ListProvider />
+      <ListProvider />
+      <ListProvider />
+      <ListProvider />
+      <ListProvider />
+      <ListProvider />
+      <ListProvider />
+      {/* {providers.map((item, key) => (
+        <ListProvider
+          key={key}
+        />
+      ))} */}
 
-            <strong>Produto</strong>
-            <p>Produto teste</p>
-
-            <strong>Telefone</strong>
-            <p>11978451348</p>
-
-            <strong>Email</strong>
-            <p>a@a.com.br</p>
-
-            <ButtonDelete>
-              <FiTrash2 size={20} color="#a8a8b3" />
-            </ButtonDelete>
-
-            <ButtonEdit>
-              <Link to="editprovider"><RiEdit2Line size={20} color="#a8a8b3" /></Link>
-            </ButtonEdit>
-          </li>
-
-          <li>
-            <strong>Nome do fornecedor</strong>
-            <p>Fornecedor teste</p>
-
-            <strong>Produto</strong>
-            <p>Produto teste</p>
-
-            <strong>Telefone</strong>
-            <p>11978451348</p>
-
-            <strong>Email</strong>
-            <p>a@a.com.br</p>
-
-            <ButtonDelete>
-              <FiTrash2 size={20} color="#a8a8b3" />
-            </ButtonDelete>
-
-            <ButtonEdit>
-              <RiEdit2Line size={20} color="#a8a8b3" />
-            </ButtonEdit>
-          </li>
-
-          <li>
-            <strong>Nome do fornecedor</strong>
-            <p>Fornecedor teste</p>
-
-            <strong>Produto</strong>
-            <p>Produto teste</p>
-
-            <strong>Telefone</strong>
-            <p>11978451348</p>
-
-            <strong>Email</strong>
-            <p>a@a.com.br</p>
-
-            <ButtonDelete>
-              <FiTrash2 size={20} color="#a8a8b3" />
-            </ButtonDelete>
-
-            <ButtonEdit>
-              <RiEdit2Line size={20} color="#a8a8b3" />
-            </ButtonEdit>
-          </li>
-
-          <li>
-            <strong>Nome do fornecedor</strong>
-            <p>Fornecedor teste</p>
-
-            <strong>Produto</strong>
-            <p>Produto teste</p>
-
-            <strong>Telefone</strong>
-            <p>11978451348</p>
-
-            <strong>Email</strong>
-            <p>a@a.com.br</p>
-
-            <ButtonDelete>
-              <FiTrash2 size={20} color="#a8a8b3" />
-            </ButtonDelete>
-
-            <ButtonEdit>
-              <RiEdit2Line size={20} color="#a8a8b3" />
-            </ButtonEdit>
-          </li>
-
-          <li>
-            <strong>Nome do fornecedor</strong>
-            <p>Fornecedor teste</p>
-
-            <strong>Produto</strong>
-            <p>Produto teste</p>
-
-            <strong>Telefone</strong>
-            <p>11978451348</p>
-
-            <strong>Email</strong>
-            <p>a@a.com.br</p>
-
-            <ButtonDelete>
-              <FiTrash2 size={20} color="#a8a8b3" />
-            </ButtonDelete>
-
-            <ButtonEdit>
-              <RiEdit2Line size={20} color="#a8a8b3" />
-            </ButtonEdit>
-          </li>
-        </ListProvider>
-      </DivListProvider>
     </Site>
   )
 }
---
+++ src/pages/ListProvider/index.js
@@ -0,0 +1,69 @@
+// Global
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+
+// Assets
+import { FiTrash2 } from 'react-icons/fi'
+import { RiEdit2Line } from 'react-icons/ri'
+
+const DivListProvider = styled.div`
+  display: flex;
+  align-itens: center;
+  justify-content: center;
+`
+
+const ListProviderul = styled.ul`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 24px;
+  list-style: none;
+  margin-bottom: 50px;
+`
+
+const ButtonDelete = styled.button`
+  position: absolute;
+  right: 24px;
+  top: 24px;
+  border: 0;
+  transition: opacity 0.2s;
+  background: transparent;
+`
+
+const ButtonEdit = styled.button`
+  position: absolute;
+  right: 60px;
+  top: 24px;
+  border: 0;
+  transition: opacity 0.2s;
+  background: transparent;
+`
+
+export default function ListProvider() {
+    return (
+        <DivListProvider>
+            <ListProviderul>
+                <li>
+                    <strong>Nome do fornecedor</strong>
+                    <p>Fornecedor teste</p>
+
+                    <strong>Produto</strong>
+                    <p>Produto teste</p>
+
+                    <strong>Telefone</strong>
+                    <p>11978451348</p>
+
+                    <strong>Email</strong>
+                    <p>a@a.com.br</p>
+
+                    <ButtonDelete>
+                        <FiTrash2 size={20} color="#a8a8b3" />
+                    </ButtonDelete>
+
+                    <ButtonEdit>
+                        <Link to="editprovider"><RiEdit2Line size={20} color="#a8a8b3" /></Link>
+                    </ButtonEdit>
+                </li>
+            </ListProviderul>
+        </DivListProvider>
+    )
+}
